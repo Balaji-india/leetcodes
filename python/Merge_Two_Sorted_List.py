31513e9305d28fb05ea0543136006564d89b6ea2
Balaji Keerthi <balajikeerthi10@gmail.com>
2026-03-23 17:25:41 +0530

    Create Merge_Two_Sorted_List.py

---
+++ python/Merge_Two_Sorted_List.py
@@ -0,0 +1,24 @@
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    
+    struct ListNode tempo;
+    struct ListNode*temp=&tempo;
+    while(list1 && list2){
+        if(list1->val<list2->val){
+            temp->next=list1;
+            list1=list1->next;
+        }
+        else{
+            temp->next=list2;
+            list2=list2->next;
+        }
+        temp=temp->next;
+    }
+    if(list1){
+        temp->next=list1;
+    }
+    else{
+        temp->next=list2;
+    }
+    return tempo.next;
+}
