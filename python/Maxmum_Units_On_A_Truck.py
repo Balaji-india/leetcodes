d1a7da69fbe8e60021d7dbd005533d5723b6ce20
Balaji Keerthi <balajikeerthi10@gmail.com>
2026-03-26 14:34:45 +0530

    Create Maxmum_Units_On_A_Truck.py

---
+++ python/Maxmum_Units_On_A_Truck.py
@@ -0,0 +1,15 @@
+def maximumUnits(boxTypes, truckSize):
+    # Sort by units per box (descending)
+    boxTypes.sort(key=lambda x: x[1], reverse=True)
+    
+    total_units = 0
+    
+    for boxes, units in boxTypes:
+        if truckSize == 0:
+            break
+        
+        take = min(boxes, truckSize)
+        total_units += take * units
+        truckSize -= take
+    
+    return total_units
