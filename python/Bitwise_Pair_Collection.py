b73061be64b2e6e032267aeb9e81aeb1a58fa969
Balaji Keerthi <balajikeerthi10@gmail.com>
2026-03-26 15:17:23 +0530

    Create Bitwise_Pair_Collection.py

---
+++ python/Bitwise_Pair_Collection.py
@@ -0,0 +1,10 @@
+def countPairs(arr):
+    count = 0
+    n = len(arr)
+    
+    for i in range(n):
+        for j in range(i+1, n):
+            if (arr[i] & arr[j]) >= (arr[i] ^ arr[j]):
+                count += 1
+    
+    return count
